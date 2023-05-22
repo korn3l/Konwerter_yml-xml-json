c719c9e726579e9fec2a76684a715b738ab82280
Kornel <dsw48766@student.dsw.edu.pl>
2023-05-22 23:41:42 +0200

    Modyfikacja konwertera o wywolanie wczesniej zaimplemetowanych funkcji

--- konwerter.py
+++ konwerter.py
@@ -19,6 +19,9 @@ else:
 input_ex = arguments.input_file.split('.')[-1]
 input_ex = input_ex.lower()
 
+output_ex = arguments.output_file.split('.')[-1]
+output_ex = output_ex.lower()
+
 if input_ex == 'json':
     with open(arguments.input_file, 'r') as file:
         try:
@@ -84,3 +87,38 @@ def xml_yaml():
     with open(arguments.output_file, "w") as file_yaml:
 	file_yaml.write(ydata)
 	file_yaml.close()
+
+
+
+if input_ex == output_ex:
+    same_ex()
+
+elif input_ex == 'json':
+    if output_ex == 'yml':
+	print("You are converting: json -> yml")
+	json_yaml()
+
+    elif output_ex == 'xml':
+	print("You are converting: json -> xml")
+	json_xml()
+
+elif input_ex == 'yml':
+    if output_ex == 'json':
+	print("You are converting yaml -> json")
+	yaml_json()
+
+    elif output_ex == 'xml':
+	print("You are converting yaml -> xml")
+	yaml_xml()
+
+elif input_ex == 'xml':
+    if output_ex == 'json':
+	print("You are converting xml -> json")
+	xml_json()
+
+    elif output_ex == 'yml':
+	print("You are converting xml -> yaml")
+	xml_yaml()
+
+else:
+    print("Something gone wrong...")
